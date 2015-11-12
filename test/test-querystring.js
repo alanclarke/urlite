557e965ddd7849b99e6210e785a5c0a938e3d9b1
alanclarke <hi@alz.so>
2015-11-12 17:19:30 +0000

    add array support + tests to querystring lib

--- test/test-querystring.js
+++ test/test-querystring.js
@@ -1,6 +1,6 @@
 /* globals describe it */
 var expect = require('expect.js')
-var qs = require('../lib/querystring')
+var qs = require('../querystring')
 describe('querystring', function () {
   describe('parse', function () {
     it('should parse a querystring', function () {
@@ -9,11 +9,17 @@ describe('querystring', function () {
     it('should handle html encoded entities', function () {
       expect(qs.parse('?a=b&amp;b=c')).to.eql({ a: 'b', b: 'c' })
     })
+    it('should handle arrays', function () {
+      expect(qs.parse('?a=b&a=c')).to.eql({ a: ['b', 'c'] })
+    })
   })
 
-  describe('format', function () {
-    it('should format a querystring', function () {
-      expect(qs.format({ a: 'b', b: 'c' })).to.eql('?a=b&b=c')
+  describe('stringify', function () {
+    it('should stringify a querystring', function () {
+      expect(qs.stringify({ a: 'b', b: 'c' })).to.eql('?a=b&b=c')
+    })
+    it('should handle arrays', function () {
+      expect(qs.stringify({ a: ['b', 'c'] })).to.eql('?a=b&a=c')
     })
   })
 })
