74f75c4ddf72136703d77ceb79e857ff2fbf09c0
alanclarke <hi@alz.so>
2015-11-12 10:41:56 +0000

    refactor format.js

--- format.js
+++ format.js
@@ -1,16 +1,20 @@
 var fragments = require('./lib/fragments')
-
 module.exports = function format (obj) {
-  var i = 0
-  var l = fragments.length
   var result = ''
-  for (i; i < l; i++) {
+  var i = fragments.length
+  while (i--) {
     var fragment = fragments[i]
     var part = obj[fragment]
-    if (part && fragment === 'protocol' && part.substr(2) !== '//') part += '//'
-    if (part && fragment === 'auth') part += '@'
-    if (part && fragment === 'port') result += ':'
-    if (part && fragment !== 'slashes') result += part
+    if (part) {
+      if (fragment === 'protocol') {
+        if (part.charAt(0) !== '/') part += '//'
+      } else if (fragment === 'auth') {
+        part += '@'
+      } else if (fragment === 'port') {
+        part = ':' + part
+      }
+      result = part + result
+    }
   }
   return result
 }
