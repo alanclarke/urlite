5c13880597d9776cace44f58aa7378766153487a
alanclarke <alan@qubit.com>
2018-08-17 10:08:22 +0100

    Update dev deps

--- Makefile
+++ Makefile
@@ -1,8 +1,4 @@
 .PHONY: test
 
-BIN = node_modules/.bin
-
 test:
-	$(BIN)/standard
-	$(BIN)/karma start --single-run
-	$(BIN)/istanbul cover $(BIN)/_mocha test/test*
+	npm test
--- karma.conf.js
+++ karma.conf.js
@@ -1,15 +1,11 @@
 module.exports = function (config) {
   config.set({
     frameworks: ['mocha'],
-    files: [ 'test/**/test-*' ],
-    preprocessors: { 'test/**/test-*': ['webpack', 'sourcemap'] },
-    webpack: {
-      watch: true,
-      devtool: 'inline-source-map'
-    },
-    webpackServer: {
-      quiet: true,
-      noInfo: true
+    files: [ 'test/test-*' ],
+    preprocessors: { '/**/*.js': ['webpack', 'sourcemap'] },
+    webpackMiddleware: {
+      stats: 'errors-only',
+      logLevel: 'error'
     },
     browsers: ['Chrome']
   })
